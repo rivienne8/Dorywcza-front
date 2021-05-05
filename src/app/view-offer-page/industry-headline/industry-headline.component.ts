ad0169b6db694b96175dbf466269573e8755f865
Joanna Rzeszotko-Doniec <rivienne8@gmail.com>
2021-05-05 14:51:55 +0200

    refactoring industry headline

--- src/app/view-offer-page/industry-headline/industry-headline.component.ts
+++ src/app/view-offer-page/industry-headline/industry-headline.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {OfferDTO} from '../../offers-page/offers-page_DTO/offerDTO';
+import {IndustryDTO} from '../../offers-page/offers-page_DTO/industry-DTO';
 
 @Component({
   selector: 'app-industry-headline',
@@ -10,11 +11,26 @@ export class IndustryHeadlineComponent implements OnInit {
 
   @Input()
   offerDTO?: OfferDTO;
+  industries: IndustryDTO[] = [];
 
   constructor() {
   }
 
   ngOnInit(): void {
+    if (this.offerDTO){
+      this.getIndustries(this.offerDTO);
+    }
+  }
+
+  getIndustries(offerDTO: OfferDTO): void{
+    let currentIndustryDTO = offerDTO.industryDTO;
+    this.industries.push(currentIndustryDTO);
+
+    while (currentIndustryDTO.parentIndustryDTO){
+      const parentIndustryDTO = currentIndustryDTO.parentIndustryDTO;
+      this.industries.unshift(parentIndustryDTO);
+      currentIndustryDTO = parentIndustryDTO;
+    }
   }
 
 }
