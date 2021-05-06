646da9a32e422212e2392e2acbc8199082b382ff
Joanna Rzeszotko-Doniec <rivienne8@gmail.com>
2021-05-06 21:40:20 +0200

    adding links for industries in industry-headline.component.html

--- src/app/offers-page/list-detail-offer/list-detail-offer.component.ts
+++ src/app/offers-page/list-detail-offer/list-detail-offer.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {OfferDTO} from '../offers-page_DTO/offerDTO';
 import {ListDetailOfferService} from './list-detail-offer.service';
 import {PageEvent} from '@angular/material/paginator';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-list-detail-offer',
@@ -9,17 +10,35 @@ import {PageEvent} from '@angular/material/paginator';
   styleUrls: ['./list-detail-offer.component.css']
 })
 export class ListDetailOfferComponent implements OnInit {
+
   @Input()
+  industryId?: number;
+
   offersDTO: OfferDTO[] = [];
   totalElements = 0;
 
 
-  constructor(private listDetailOfferService: ListDetailOfferService) { }
+  constructor(private listDetailOfferService: ListDetailOfferService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getServiceOffersPagination(0, 10);
+    // this.getIndustryId();
+    if (!this.industryId){
+      this.getServiceOffersPagination(0, 10);
+    } else {
+      this.getServiceOffersPaginationForIndustry(this.industryId, 0, 10);
+    }
+
 
   }
+
+  // getIndustryId(): void {
+  //   const param = 'industryId';
+  //   this.route.queryParams.subscribe(
+  //     params => {
+  //       this.industryId =  params[param];
+  //     });
+  // }
 
   getServiceOffersPagination(page: number, size: number): void {
     this.listDetailOfferService.getServiceOffersPagination(location.pathname, page.toString(), size.toString())
@@ -31,5 +50,13 @@ export class ListDetailOfferComponent implements OnInit {
 
   nextPage(event: PageEvent): void {
     this.getServiceOffersPagination(event.pageIndex, event.pageSize);
+  }
+
+  getServiceOffersPaginationForIndustry(industryId: number, page: number, size: number): void {
+    this.listDetailOfferService.getOffersForIndustry(location.pathname, industryId.toString(), page.toString(), size.toString())
+      .subscribe(serviceOfferDTO => {
+        this.offersDTO = serviceOfferDTO.content;
+        this.totalElements = serviceOfferDTO.totalElements;
+      });
   }
 }
